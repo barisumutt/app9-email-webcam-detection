c335d2b088f6fec53ebb9ef278e35c7258e1e520
barisumut <barisumutbaykal@gmail.com>
2023-05-21 22:04:39 +0300

    Send email attachment when webcam detects an object

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import glob
 import cv2
 import time
 from emailing import send_email
+import os
 
 
 video = cv2.VideoCapture(0)
@@ -11,6 +12,14 @@ time.sleep(1)
 first_frame = None
 status_list = []
 count = 1
+
+
+def clean_folder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+
+
 while True:
     status = 0
     check, frame = video.read()
@@ -49,7 +58,8 @@ while True:
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email(image_with_object, )
+        send_email(image_with_object)
+        clean_folder()
 
     print(status_list)
 
